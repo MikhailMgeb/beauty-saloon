194cd4d253b4238b2a5e66ac1f5a345bc98c8b1b
Michael Mgebrishvili <mmd920802@gmail.com>
2023-12-09 16:17:03 +0300

    startboard project salon

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import './index.css';
+
+const message: string = 'app started';
+console.log(message);
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    extensionAlias: {
+      '.js': ['.js', '.ts'],
+      '.cjs': ['.cjs', '.cts'],
+      '.mjs': ['.mjs', '.mts'],
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.([cm]?ts)$/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html',
+  }), new MiniCssExtractPlugin()],
+};
